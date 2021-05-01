6a2de8876a79c63e7fdc04a25009acd4b10929ac
tpisfil <tpisfil13@gmail.com>
2021-04-30 19:22:41 -0500

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeName(){
+    document.querySelector("#name").innerText = "John Snow";
+}
+
+function disappear(){
+    document.getElementById("bye").style.visibility="hidden";
+}
+
+function disappear2(){
+    document.getElementById("bye2").style.visibility="hidden";
+}
